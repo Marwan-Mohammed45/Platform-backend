cd08e9160ce41c6da004e1fb0a7891ee69fa14fd
Marwan-Mohammed45 <marwan.3zazy@gmail.com>
2025-11-05 22:02:24 +0200

    first commit

--- Backend/App.js
+++ Backend/App.js
@@ -1,26 +1,28 @@
 import dotenv from "dotenv";
-import Express from "express";
+import express from "express";
 import connectdb from "./DB/ConnectDB.js";
-import AuthRouthes from "./routes/auth.routes.js"
+import AuthRoutes from "./routes/auth.routes.js";
 import cors from "cors";
 
-
 dotenv.config();
-const app =Express()
-const Port =process.env.Port||4000;
-app.use(Express.json());
-app.use(cors());
-app.use("/api",AuthRouthes);
 
-app.get("/",(req,res) =>{
-    res.send("Server is working ✅ ")
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api", AuthRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Server is working ✅");
 });
+
 connectdb();
 
+app.listen(PORT, () => {
+  console.log(`Platform is working on port ${PORT} ☝️🚀`);
+});
 
-app.listen(Port,() => {
-    console.log(`Platform is working on  ${Port} ☝️ 🚀`);
-})
-
-
-
+export default app;
